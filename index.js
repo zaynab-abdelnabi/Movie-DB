862b7ac6d1a721817e35f36255adbbd56ca794a1
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2021-12-28 14:58:52 +0200

    step 3

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/test' , (req, res) => {
+    res.send({status:200,message:"Ok"})
+})
+
+app.get('/time' , (req, res) => {
+    let date = new Date();
+    res.send({status:200,message:`${date.getHours()}:${date.getMinutes()}`})
+})
+
 app.get(/\//, (req, res) => {
   res.send('Ok')
 })
