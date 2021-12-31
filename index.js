daf7457833e3831a5c3289c277a4535372b7451c
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2021-12-31 11:41:02 +0200

    step 10 and fix step 8

--- index.js
+++ index.js
@@ -60,21 +60,15 @@ app.get('/movies/create' , (req, res) => {
         res.status(403).send({status:403, error:true, message:'The year provided is not valid'})
     }
     
-    else if(!req.query.rating || req.query.rating > 10 || req.query.rating < 0){
-        let newMovie = {
-            title: req.query.title,
-            year: req.query.year,
-            rating: 4
-        }
-        movies.push(newMovie);
-        res.status(200).send({status:200, data: movies})
+    else if(req.query.rating && (req.query.rating > 10 || req.query.rating < 0) ){
+        res.status(403).send({status:403, error:true, message:'The rating provided is not valid'})
     }
     
     else {
         let newMovie = {
             title: req.query.title,
             year: req.query.year,
-            rating: req.query.rating
+            rating: `${req.query.rating || 4}`
         }
         movies.push(newMovie);
         res.status(200).send({status:200, data: movies})
@@ -112,8 +106,46 @@ app.get(['/movies/read/id/:id','/movies/read/id/' ], (req, res) => {
     }
 })
 
-app.get('/movies/update' , (req, res) => {
-    res.send()
+app.get(['/movies/update', '/movies/update/:id'] , (req, res) => {
+
+    if(req.params.id){
+        
+        if(Number(req.params.id) >= 0 && req.params.id < movies.length){
+
+            if(!req.query.title && !req.query.year && !req.query.rating){
+                res.status(404).send({status:404, error:true, message:`Enter the data you want to update`})
+            }
+
+            else if(req.query.year && ( req.query.year > new Date().getFullYear() || req.query.year < 1895 || req.query.year.length != 4 || isNaN(req.query.year) )){
+                res.status(403).send({status:403, error:true, message:'The year provided is not valid'})
+            }
+            
+            else if(req.query.rating && (isNaN(req.query.rating) || req.query.rating >10 || req.query.rating < 0)){
+                res.status(403).send({status:403, error:true, message:'The rating provided is not valid'})
+            }
+            else{
+                let editedMovie={
+                    title : `${req.query.title || movies[req.params.id].title }`,
+                    year : `${req.query.year || movies[req.params.id].year}`,
+                    rating : `${req.query.rating || movies[req.params.id].rating}`
+                }
+                movies.splice(req.params.id, 1, editedMovie)
+    
+    
+                res.status(200).send({status:200, data: movies[req.params.id]})
+            
+            }
+
+        }
+        else{
+            res.status(404).send({status:404, error:true, message:`The movie ${req.params.id} does not exist`})
+        }
+    }
+
+    else{
+        res.status(404).send({status:404, error:true, message:`Enter the id of the movie`})
+    }
+
 })
 
 app.get(['/movies/delete/:id','/movies/delete' ] , (req, res) => {
@@ -123,7 +155,7 @@ app.get(['/movies/delete/:id','/movies/delete' ] , (req, res) => {
             movies.splice(req.params.id, 1);
             res.status(200).send({status:200, data: movies})
         }
-        
+
         else{
             res.status(404).send({status:404, error:true, message:`The movie ${req.params.id} does not exist`})
         }
